73286f491ecf93545ee82b075326f5bf687e1db9
Michael Dorner <mail@michaeldorner.de>
2016-12-27 11:50:55 +0100

    updated headers

--- BeeTee Demo/AppDelegate.swift
+++ BeeTee Demo/AppDelegate.swift
@@ -1,10 +1,6 @@
-//
-//  AppDelegate.swift
-//  BeeTee
-//
-//  Created by Michael Dorner on 22.12.16.
-//  Copyright © 2016 Michael Dorner. All rights reserved.
-//
+/*
+ This file is part of BeeTee Project. It is subject to the license terms in the LICENSE file found in the top-level directory of this distribution and at https://github.com/michaeldorner/BeeTee/blob/master/LICENSE. No part of BeeTee Project, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the LICENSE file.
+ */
 
 import UIKit
 
